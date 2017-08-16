b7d380081725a573d77004422a5a4618d71eb9e9
bernadette privateer <lynprivateer@gmail.com>
2017-08-16 19:17:03 -0400

    final page functional

--- routes/index.js
+++ routes/index.js
@@ -11,20 +11,15 @@ router.get("/", function(req, res){
 router.post("/tasks", function(req, res){
   let task = req.body.tasks;
   let unDone = req.body.incomplete;
-
-
+  
   if(task){
   list.push(task);
   console.log(list);
 } else {
+  list.splice(list.indexOf(unDone),1)
   complete.push(unDone);
 
-}
-
-
-
-
-// req.checkBody("tasks", "cannot be empty").notEmpty();
+}// req.checkBody("tasks", "cannot be empty").notEmpty();
   // let errors = req.getValidationResult();
   // let messages = [];
   // let task = req.body.tasks
